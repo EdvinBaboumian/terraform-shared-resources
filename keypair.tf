586cbebdc523c50a4cfd1dda1ce2f1b66bc924cc
EdvinBaboumian <109412509+EdvinBaboumian@users.noreply.github.com>
2026-08-01 14:49:14 +0400

    Delete keypair.tf

--- keypair.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "dave-key" {
-  key_name   = "dave-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINVU5tGm02B8UIWtMM208ftSGfdsLMHcoXEUcKeB8Q/9 Edvin@DESKTOP-JV6H3IK"
-}
